f1532378557fcda94ee29072cea4494ea79f8e5b
HarrisonIbarra <116966095+HarrisonIbarra@users.noreply.github.com>
2023-07-21 20:55:07 -0500

    Add files via upload

---
+++ app_webside/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppWebsideConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_webside'
---
+++ app_webside/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+
+
+
+
+def home(request):
+    return render(request, 'home.html', {})
+
+
+
+# Create your views here.
